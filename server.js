68e5ca2d59e9bdc1e353513d7f9c6d58f8cfbdec
Dima Kurbatov <dskurbatov13@gmail.com>
2017-04-23 13:42:06 -0700

    the http/https modules

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-import config, { nodeEnv, logStar } from './config';
+import http from 'http';
 
-console.log(config, nodeEnv);
+const server = http.createServer((req, res) => {
+	res.write("Hello There!!!\n");
+	setTimeout(()=>{
+		res.write("I can Stream!!!\n");
+		res.end();
+	}, 3000);
+});
 
-logStar("hello there");
+server.listen(8080);
